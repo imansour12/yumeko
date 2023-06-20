13aca8f6bac0bd3c40c34838be9f2cb6056156a1
Mohamed Iyed Mansour <iyed@pengguin.com>
2023-06-20 13:32:01 +0100

    Find Devices

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import pychromecast
 
 parser = argparse.ArgumentParser(prog="Yumeko" , description="An alarm system that connects to a chromecast device and plays a sound when triggered.",epilog="Iyed")
 parser.add_argument('-d', "--device", help="The chromecast device name", required=False)
@@ -8,8 +9,17 @@ parser.add_argument('-s',  "--sound", help="The sound to play when the alarm is 
 
 args = parser.parse_args()
 
+def choose_device():
+    # Discover available Chromecast devices
+    services, browser = pychromecast.discovery.discover_chromecasts()
+    # Print the services found
+    print(services)
+
 def main():
-    print(args.device)
+    if args.device is None:
+        device = choose_device()
+        print(device)
+
 
 if __name__ == "__main__":
     main()
