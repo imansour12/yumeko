e2d20218e97e993e8517a8bc04880e2e8c3e76bd
Mohamed Iyed Mansour <iyedmansour0@gmail.com>
2023-06-23 11:47:05 +0100

    Add choose device

--- main.py
+++ main.py
@@ -9,11 +9,43 @@ parser.add_argument('-s',  "--sound", help="The sound to play when the alarm is 
 
 args = parser.parse_args()
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def choose_device():
-    # Discover available Chromecast devices
+    print(f"{bcolors.OKBLUE}Searching for chromecast devices on the network...{bcolors.ENDC}")
     services, browser = pychromecast.discovery.discover_chromecasts()
-    # Print the services found
     print(services)
+    print(browser.services)
+    services = [x for x in services if x is not None]
+    if len(services) == 0:
+        print(f"{bcolors.FAIL}No chromecast devices found on the network.{bcolors.ENDC}")
+        sys.exit(1)
+    print(f"{bcolors.OKGREEN}Found {len(services)} chromecast devices on the network.{bcolors.ENDC}")
+    #loop through each of them with an index and ask user to choose
+    for i, service in enumerate(services):
+        print(f"{bcolors.OKCYAN}[{i}] {service.friendly_name}{bcolors.ENDC}")
+    print(f"{bcolors.OKBLUE}Choose a chromecast device by entering its index number:{bcolors.ENDC}")
+    while True:
+        try:
+            index = int(input())
+            if index < 0 or index >= len(services):
+                raise ValueError
+            break
+        except ValueError:
+            print(f"{bcolors.FAIL}Invalid index number. Try again:{bcolors.ENDC}")
+    return services[index]
+
+
 
 def main():
     if args.device is None:
